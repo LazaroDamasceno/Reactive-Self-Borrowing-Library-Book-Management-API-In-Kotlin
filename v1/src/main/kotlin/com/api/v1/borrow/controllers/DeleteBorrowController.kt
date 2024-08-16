a7ca6039c8698908a86fa4f4c888a27773195ef6
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-15 23:38:47 -0300

    Created the controller DeleteBorrowController

--- v1/src/main/kotlin/com/api/v1/borrow/controllers/DeleteBorrowController.kt
+++ v1/src/main/kotlin/com/api/v1/borrow/controllers/DeleteBorrowController.kt
@@ -1,9 +1,12 @@
 package com.api.v1.borrow.controllers
 
-import com.api.v1.borrow.services.DeleteAllBorrowsService
+import com.api.v1.annotations.ISBN
+import com.api.v1.annotations.SSN
+import com.api.v1.borrow.services.DeleteBorrowService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
@@ -11,15 +14,15 @@ import reactor.core.publisher.Mono
 
 @RestController
 @RequestMapping("api/v1/borrows")
-internal class DeleteAllBorrowsController {
+internal class DeleteBorrowController {
 
     @Autowired
-    private lateinit var service: DeleteAllBorrowsService
+    private lateinit var service: DeleteBorrowService
 
-    @DeleteMapping
+    @DeleteMapping("by-book/{isbn}/and/by-borrower/{ssn}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    fun deleteAll(): Mono<Void> {
-        return service.deleteAll()
+    fun delete(@ISBN @PathVariable isbn: String, @SSN @PathVariable  ssn: String): Mono<Void> {
+        return service.delete(isbn, ssn)
     }
 
 }
